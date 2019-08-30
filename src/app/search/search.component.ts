9c8d65f902122b1425304c72c18db873bb108d06
Julia-Agasaro <agasarojulia@gmail.com>
2019-08-30 13:01:58 +0200

    Imported classes

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -2,18 +2,46 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { User } from '../user-class/user';
 import { UserRequestService } from '../user-http/user-request.service';
+import { environment } from '../../environments/environment';
+
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
-  user:User;
-  constructor( private userService:UserRequestService) { }
+  // user:User;
+userName: string ;
+response: any;
+  // constructor( private userService:UserRequestService) { }
+  // userName = "";
+  // githubData: any = "";
+  // imgavatarUrl = "./assets/avatar.png";
+  constructor(private http: HttpClient) { }
+  // constructor( private userService:UserRequestService) { }
+  // searchUser() {
 
-  ngOnInit() {
-    this.userService.userRequest()
-    this.user = this.userService.user
+    // this.http.get("https://api.github.com/users/" + this.userName + "?access_token=" + environment.access_token)
+    //   .subscribe(
+  //       (response: Response) => {
+  //         const user = response.json();
+  //         this.githubData = user;
+          
+  //         this.imgavatarUrl = user.avatar_url;
+  //       }
+  //     )
+  // }
+
+
+    ngOnInit() {
+    //  
+  }
+  search(){
+    this.http.get('https://api.github.com/users/'+ this.userName)
+     .subscribe((response)=>{
+       this.response = response;
+       console.log(this.response);
+     })
   }
 
 }
